e9caf62304aa03bcd59e92c2016089496cc87b1e
vaibhavAccionLabs <vaibhav.katariya@accionlabs.com>
2020-07-29 15:39:28 +0530

    Updates

---
+++ precache-manifest.2378ba48a10e656eb42d6d31b1816e59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f28e65b5b19d79e6da49764744aaa8",
+    "url": "/twitterApp/index.html"
+  },
+  {
+    "revision": "a2589c2ac8423581772e",
+    "url": "/twitterApp/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "da3ce5df4b349fe69f96",
+    "url": "/twitterApp/static/css/main.fa0a8f16.chunk.css"
+  },
+  {
+    "revision": "a2589c2ac8423581772e",
+    "url": "/twitterApp/static/js/2.b0bf7acd.chunk.js"
+  },
+  {
+    "revision": "8f6383b81ee6aba9e7b4",
+    "url": "/twitterApp/static/js/3.0ab29dce.chunk.js"
+  },
+  {
+    "revision": "da3ce5df4b349fe69f96",
+    "url": "/twitterApp/static/js/main.09277ea8.chunk.js"
+  },
+  {
+    "revision": "f6f948ce83e32abbc815",
+    "url": "/twitterApp/static/js/runtime~main.4de8aba4.js"
+  }
+]);
